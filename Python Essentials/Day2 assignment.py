17e4dd5989c422b1edb7a2e46ca7aa3e83e8a819
blockerpiyush <66992776+blockerpiyush@users.noreply.github.com>
2020-09-04 10:01:00 +0000

    Add files via upload

---
+++ Python Essentials/Day2 assignment.py
@@ -0,0 +1,62 @@
+#Question1
+a = ["apple", "banana"]
+print(a)
+a.append("orange")
+print(a)
+a.extend(["grapes","lichi"])
+print(a)
+a.insert(1, "mango")
+print(a)
+a.remove("banana")
+print(a)
+a.pop()
+print(a)
+
+#Question2
+b={'PB':'Peanut Butter','PJ':'Pajamas'}
+print(b)
+l =len(b)
+print(l)
+print(b.keys())
+print(b.values())
+print(b.items())
+print(b.clear())
+
+#Question3
+c = {1,3,5, "Hello"}
+print(c)
+c.add(4)
+print(c)
+c.update([2,3,4])
+print(c)
+c.discard(3)
+print(c)
+c.remove(2)
+print(c)
+c.clear()
+print(c)
+
+#Question4
+d =(99,95,90,89,93,96)
+print(d)
+l = len(d)
+print(l)
+print(max(d))
+print(min(d))
+print(sorted(d))
+print(d.index(90))
+
+#Question5
+e = "My name is Piyush"
+print(e)
+l = len(e)
+print(l)
+print(e.lower())
+print(e.upper())
+print(e.isupper())
+print(e.split(" "))
+
+
+
+
+
