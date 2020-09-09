ca734a4485b867ea83f47af3931b8f9d91dbdf51
blockerpiyush <66992776+blockerpiyush@users.noreply.github.com>
2020-09-09 09:30:51 +0000

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,19 @@
+function changeImage1() {
+    const ele = document.getElementById("image");
+    const newUrl =
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdncache-mauganscorp.netdna-ssl.com%2Fthumbseg%2F2079%2F2079614-bigthumbnail.jpg&f=1&nofb=1";
+    ele.src = newUrl;
+    console.log("working")
+  }
+  function changeImage2() {
+    const ele = document.getElementById("image");
+    const newUrl =
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperplay.com%2Fwalls%2Ffull%2F3%2F8%2F4%2F195030.jpg&f=1&nofb=1";
+    ele.src = newUrl;
+  }
+  function changeImage3() {
+    const ele = document.getElementById("image");
+    const newUrl =
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.K6mrvG3shxyhdEG5KAOsRAHaFd%26pid%3DApi&f=1";
+    ele.src = newUrl;
+  }
---
+++ Q2.js
@@ -0,0 +1,7 @@
+function myfunction() { 
+  var x =  
+      document.getElementById("myText").value; 
+  
+  document.getElementById( 
+    "demo").innerHTML = x; 
+  }
